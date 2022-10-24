9933399176d8cce6e5fefec7d34df4a7bd8798e8
Jhon Rivera <jhonrivego@gmail.com>
2022-10-24 14:17:14 -0500

    Se realizo conexion a base de datos Mongoose

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 require('./config/config');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+
+//Conectado a la base de datos Moongo BD
+mongoose.connect('mongodb://localhost:27017/todo', (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
+
 
 
 
