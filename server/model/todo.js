f63230913b231f1611cbb9d53b22c5e889642fcf
Jhon Rivera <jhonrivego@gmail.com>
2022-10-24 17:27:38 -0500

    Se creo modelo Todo

---
+++ server/model/todo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
+
+const todoSchema = new Schema({
+
+    tarea: {
+        type: String,
+        uniqued: true,
+        required: [true, 'Tarea Requerido']
+    },
+
+    categoria: {
+        type: String,
+        required: [true, 'Categoria Requerida']
+    },
+
+    prioridad: {
+        type: String,
+        required: [true, 'Prioridad Requerida']
+    },
+    estado: {
+        type: String,
+        default: 'EN PROGRESO'
+    }
+
+});
+
+userSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser unico' });
+
+module.exports = mongoose.model('Todo', todoSchema);
